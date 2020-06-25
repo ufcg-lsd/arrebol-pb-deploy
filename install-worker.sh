b29a9241531c24bc746090a45b76778c4d137353
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2020-06-25 16:59:11 -0300

    addressing requested changes

--- install-worker.sh
+++ install-worker.sh
@@ -15,15 +15,13 @@ ANSIBLE_CFG_FILE="$(pwd)/ansible/worker/ansible.cfg"
 
 # setup hosts
 WORKER_HOST_IP=$(grep ^worker_host $HOSTS_CONF_FILE | awk -F "=" '{print $2}')
-PATTERN_HELPER="\[worker-machine\]"
-WORKER_HOST_IP_PATTERN=$(grep -A1 $PATTERN_HELPER $ANSIBLE_HOSTS_FILE | tail -n 1)
 echo $WORKER_HOST_IP_PATTERN
-sed -i "s/$WORKER_HOST_IP_PATTERN/$WORKER_HOST_IP/" $ANSIBLE_HOSTS_FILE
+sed -i '/\[worker-machine\]/,/\[worker-machine:vars\]/{//!d}' ${ANSIBLE_HOSTS_FILE}
+sed -i "/\[worker-machine:vars\]/i ${WORKER_HOST_IP}" ${ANSIBLE_HOSTS_FILE}
 
 WORKER_NODE_HOST_IP=$(grep ^worker_node_host $HOSTS_CONF_FILE | awk -F "=" '{print $2}')
-PATTERN_HELPER="\[worker-node-machine\]"
-WORKER_NODE_HOST_IP_PATTERN=$(grep -A1 $PATTERN_HELPER $ANSIBLE_HOSTS_FILE | tail -n 1)
-sed -i "s/$WORKER_NODE_HOST_IP_PATTERN/$WORKER_NODE_HOST_IP/" $ANSIBLE_HOSTS_FILE
+sed -i '/\[worker-node-machine\]/,/\[worker-node-machine:vars\]/{//!d}' ${ANSIBLE_HOSTS_FILE}
+sed -i "/\[worker-node-machine:vars\]/i ${WORKER_NODE_HOST_IP}" ${ANSIBLE_HOSTS_FILE}
 
 ANSIBLE_SSH_PRIVATE_KEY_FILE=$(grep ^private_key_path $HOSTS_CONF_FILE | awk -F "=" '{print $2}')
 PRIVATE_KEY_FILE_PATH_PATTERN="ansible_ssh_private_key_file"
@@ -34,4 +32,4 @@ REMOTE_USER=$(grep ^remote_host_user $HOSTS_CONF_FILE | awk -F "=" '{print $2}')
 PATTERN_HELPER="remote_user"
 sed -i "s#.*$PATTERN_HELPER = .*#$PATTERN_HELPER = $REMOTE_USER#g" $ANSIBLE_CFG_FILE
 
-cd ansible/worker && ansible-playbook -vvv deploy.yml
+#cd ansible/worker && ansible-playbook -vvv deploy.yml
